f72f661abb40c39d3cf08f627be1f6165ce7dd8a
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-22 21:41:08 +0300

    phonePattern

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -79,7 +79,7 @@ const ContactForm = () => {
         name="number"
         value={number}
         onChange={handleNumberChange}
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        phonePattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
